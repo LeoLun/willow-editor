e6c37405d5a362fd5c2613c51c5a887a8f7ec051
LeoLun <helloleolun@gmail.com>
2023-01-11 22:51:03 +0800

    fix: 修复错误

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { createApp } from 'vue';
 import { registerSW } from 'virtual:sw-plugin';
 import SvgIcon from '@/components/svg-icon/index.vue';
-// import ToastService from '@/components/toast/index';
-// import DialogService from '@/components/dialog/index';
-// import {
-//   IToastService,
-//   IDialogService,
-// } from '@/common/const';
+import ToastService from '@/components/toast/index';
+import DialogService from '@/components/dialog/index';
+import {
+  IToastService,
+  IDialogService,
+} from '@/common/const';
 
 import App from './app.vue';
 
@@ -19,6 +19,6 @@ registerSW('sw.js', {
 }).catch(console.error);
 const app = createApp(App);
 app.component('SvgIcon', SvgIcon);
-// app.provide(IToastService, ToastService);
-// app.provide(IDialogService, DialogService);
+app.provide(IToastService, ToastService);
+app.provide(IDialogService, DialogService);
 app.mount('#app');
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,7 +4,7 @@ import { inject, InjectionKey } from 'vue';
 export function requireInjection<T>(key: InjectionKey<T>, defaultValue?: T) {
   const resolved = inject(key, defaultValue);
   if (!resolved) {
-    throw new Error(`${key} was not provided.`);
+    throw new Error(`${key.toString()} was not provided.`);
   }
   return resolved;
 }
